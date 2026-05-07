732517a3cfb2b2854010e822f15e9953366ab4fe
Reyan149 <reyan.rhea2012@gmail.com>
2026-05-07 17:19:02 +0100

    updates

---
+++ lesson-2-data_type/act1.py
@@ -0,0 +1,16 @@
+name = 'Reyan'
+age = 11
+isStudent = True
+weight = 40
+
+print("Name:", name)
+print(f"Datatype of name: {type(name)}")
+
+print("Age:", age)
+print(f"Datatype of age: {type(age)}")
+
+print("Is Student:", isStudent)
+print(f"Datatype of isStudent: {type(isStudent)}")
+
+print("Weight:", weight)
+print(f"Datatype of weight: {type(weight)}")
