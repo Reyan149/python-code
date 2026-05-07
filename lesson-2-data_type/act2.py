485df3eecc59010985716d9438de0ba293017a8a
Reyan149 <reyan.rhea2012@gmail.com>
2026-05-07 17:36:21 +0100

    updates

---
+++ lesson-2-data_type/act2.py
@@ -0,0 +1,30 @@
+num1 = 48
+num2 = 86
+
+print("Number 1", num1)
+print("Number 2", num2)
+
+#Addition
+print("Addition:", num1 + num2)
+
+#Subtraction
+print("Subtraction:", num1 - num2)
+
+#Multiplication
+print("Multiplication:", num1 * num2)
+
+#Division  
+print("Division:", num1 / num2)
+
+#Comparison operators
+#equality
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+#Expression
+result = (num1/2) + (num2 * 3) - 10
+print(result)
