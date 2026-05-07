371a908a46093f7e5baa3a281c220166fdf66903
Reyan149 <reyan.rhea2012@gmail.com>
2026-05-07 17:50:19 +0100

    updates

---
+++ lesson-2-data_type/act3.py
@@ -0,0 +1,14 @@
+first_name = 'Reyan'
+last_name = 'Pandit'
+
+#String conctanation
+full_name = first_name + last_name
+print(full_name)
+
+print(first_name * 5)
+
+word = 'Python'
+print(f"Length of {word} is : {len(word)}")
+print(f"First character of {word} is : {word[0]}")
+print(f"Last character of {word} is : {word[-1]}")
+print(f"Slicing of {word} from index 1 to 4 is : {word[1:3]}")
